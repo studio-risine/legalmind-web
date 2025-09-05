4fb0f86436e530704e897528008e46f4debb0d0e
Gabriel Melo <gblsmlo@gmail.com>
2025-09-05 19:56:52 -0300

    refactor: rename _tag to tag in Either type for consistency

--- src/utils/either.ts
+++ src/utils/either.ts
@@ -1,23 +1,23 @@
 export type Either<L, R> = Left<L> | Right<R>
 
 export interface Left<L> {
-	readonly _tag: 'Left'
+	readonly tag: 'left'
 	readonly left: L
 }
 
 export interface Right<R> {
-	readonly _tag: 'Right'
+	readonly tag: 'right'
 	readonly right: R
 }
 
-export const left = <L>(value: L): Left<L> => ({ _tag: 'Left', left: value })
+export const left = <L>(value: L): Left<L> => ({ tag: 'left', left: value })
 export const right = <R>(value: R): Right<R> => ({
-	_tag: 'Right',
+	tag: 'right',
 	right: value,
 })
 
 export const isLeft = <L, R>(either: Either<L, R>): either is Left<L> =>
-	either._tag === 'Left'
+	either.tag === 'left'
 
 export const isRight = <L, R>(either: Either<L, R>): either is Right<R> =>
-	either._tag === 'Right'
+	either.tag === 'right'
