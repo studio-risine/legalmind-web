8bfc735cb210c77403e5b6515d1e27cef8bca1ff
Gabriel Melo <gblsmlo@gmail.com>
2025-09-05 22:54:52 -0300

    feat: add middleware for session management and refactor supabase clients

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,6 +1,9 @@
 import { createServerClient } from '@supabase/ssr'
 import { type NextRequest, NextResponse } from 'next/server'
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
 const PUBLIC_ROUTES = [
 	{ path: '/', exact: true },
 	{ path: '/auth', exact: false },
@@ -25,76 +28,39 @@ export async function updateSession(request: NextRequest) {
 		request,
 	})
 
-	const supabase = createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-		{
-			cookies: {
-				getAll() {
-					return request.cookies.getAll()
-				},
-				setAll(cookiesToSet) {
-					// biome-ignore lint/suspicious/useIterableCallbackReturn: <explanation>
-					cookiesToSet.forEach(({ name, value, options }) =>
-						request.cookies.set(name, value),
-					)
-					supabaseResponse = NextResponse.next({
-						request,
-					})
-					// biome-ignore lint/suspicious/useIterableCallbackReturn: <explanation>
-					cookiesToSet.forEach(({ name, value, options }) =>
-						supabaseResponse.cookies.set(name, value, options),
-					)
-				},
+	const supabase = createServerClient(supabaseUrl!, supabaseKey!, {
+		cookies: {
+			getAll() {
+				return request.cookies.getAll()
+			},
+			setAll(cookiesToSet) {
+				// biome-ignore lint/suspicious/useIterableCallbackReturn: <explanation>
+				cookiesToSet.forEach(({ name, value, options }) =>
+					request.cookies.set(name, value),
+				)
+				supabaseResponse = NextResponse.next({
+					request,
+				})
+				// biome-ignore lint/suspicious/useIterableCallbackReturn: <explanation>
+				cookiesToSet.forEach(({ name, value, options }) =>
+					supabaseResponse.cookies.set(name, value, options),
+				)
 			},
 		},
-	)
+	})
 
 	const {
 		data: { user },
 	} = await supabase.auth.getUser()
 
 	const hasAuthenticated = user?.aud === 'authenticated'
+
 	if (!hasAuthenticated && !isPublic && !isError) {
 		const url = request.nextUrl.clone()
 		url.pathname = '/auth/login'
 
 		return NextResponse.redirect(url)
 	}
-
-	// if (user && request.nextUrl.pathname.startsWith('/auth')) {
-	// 	const url = request.nextUrl.clone()
-	// 	url.pathname = '/dashboard'
-
-	// 	return NextResponse.redirect(url)
-	// }
-
-	// if (user && !isPublic) {
-	// 	const url = request.nextUrl.clone()
-	// 	url.pathname = '/dashboard'
-
-	// 	return NextResponse.redirect(url)
-	// }
-
-	// if (!user && !isPublic && !request.nextUrl.pathname.startsWith('/error')) {
-	// 	// no user, potentially respond by redirecting the user to the login page
-	// 	const url = request.nextUrl.clone()
-	// 	url.pathname = '/auth/login'
-	// 	return NextResponse.redirect(url)
-	// }
-
-	// IMPORTANT: You *must* return the supabaseResponse object as it is.
-	// If you're creating a new response object with NextResponse.next() make sure to:
-	// 1. Pass the request in it, like so:
-	//    const myNewResponse = NextResponse.next({ request })
-	// 2. Copy over the cookies, like so:
-	//    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-	// 3. Change the myNewResponse object to fit your needs, but avoid changing
-	//    the cookies!
-	// 4. Finally:
-	//    return myNewResponse
-	// If this is not done, you may be causing the browser and server to go out
-	// of sync and terminate the user's session prematurely!
 
 	return supabaseResponse
 }
