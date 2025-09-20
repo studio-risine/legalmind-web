2008408c52a29aa110548bea80e5e3a0fb1dd3c6
Gabriel Melo <gblsmlo@gmail.com>
2025-09-20 15:53:50 -0300

    refactor: update Supabase client configuration to use PUBLISHABLE_OR_ANON_KEY

--- src/libs/supabase/client.ts
+++ src/libs/supabase/client.ts
@@ -1,10 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY
 
 export const createClient = () => {
 	return createBrowserClient(supabaseUrl!, supabaseKey!)
 }
-
-export const supabase = createClient()
