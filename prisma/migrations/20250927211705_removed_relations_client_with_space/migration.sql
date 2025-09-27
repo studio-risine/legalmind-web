5d020abb8a6b9fc45526ef6ab01e16f94eb4d68c
Gabriel Melo <gblsmlo@gmail.com>
2025-09-27 19:23:16 -0300

    feat: add initial Prisma schema with models, enums, and migrations

---
+++ prisma/migrations/20250927211705_removed_relations_client_with_space/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "public"."clients" DROP CONSTRAINT "clients_space_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."processes" DROP CONSTRAINT "processes_assigned_lawyer_id_fkey";
