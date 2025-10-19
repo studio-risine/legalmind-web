05efaf1b23fdf06ffccc409e3d2294e67a4ea6c8
Gabriel Melo <gblsmlo@gmail.com>
2025-10-19 20:50:07 -0300

    feat(ui): update app layout and dashboard modules

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import type { ReactNode } from 'react'
+import { queryClient } from '@/libs/react-query-client'
 
 export function Providers({ children }: { children: ReactNode }) {
 	return (
@@ -12,7 +14,7 @@ export function Providers({ children }: { children: ReactNode }) {
 			disableTransitionOnChange
 			enableColorScheme
 		>
-			{children}
+			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 		</NextThemesProvider>
 	)
 }
