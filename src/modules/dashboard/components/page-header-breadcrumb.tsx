88a330c73c0fccd8a2d68bcec61b9d01dd7b4118
Gabriel Melo <gblsmlo@gmail.com>
2025-10-21 10:56:33 -0300

    fix(ui): improve breadcrumb and sidebar styles for better layout consistency

--- src/modules/dashboard/components/page-header-breadcrumb.tsx
+++ src/modules/dashboard/components/page-header-breadcrumb.tsx
@@ -21,7 +21,7 @@ export function PageHeaderWithBreadcrumb({
 	children,
 }: PageHeaderBreadcrumbProps) {
 	return (
-		<div className="py- flex items-center justify-between px-6 py-4">
+		<div className="flex items-center justify-between border-b px-4 py-2">
 			<Breadcrumb>
 				<BreadcrumbList>
 					{breadcrumb.map((item, index) => {
