6045f22ac68487578614d04d3bc10c72da4925ff
Gabriel Melo <gblsmlo@gmail.com>
2025-10-21 10:55:28 -0300

    fix(setup): update Supabase client initialization to use env variables

--- src/libs/supabase/server.ts
+++ src/libs/supabase/server.ts
@@ -1,30 +1,32 @@
 'use server'
 
+import { env } from '@infra/env'
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY
 
 export async function createClient() {
 	const cookieStore = await cookies()
 
-	return createServerClient(supabaseUrl!, supabaseKey!, {
-		cookies: {
-			getAll() {
-				return cookieStore.getAll()
-			},
-			setAll(cookiesToSet) {
-				try {
-					cookiesToSet.forEach(({ name, value, options }) =>
-						cookieStore.set(name, value, options),
-					)
-				} catch {
-					// The `setAll` method was called from a Server Component.
-					// This can be ignored if you have middleware refreshing
-					// user sessions.
-				}
+	return createServerClient(
+		env.NEXT_PUBLIC_SUPABASE_URL || '',
+		env.SUPABASE_SCRET_KEY,
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll()
+				},
+				setAll(cookiesToSet) {
+					try {
+						cookiesToSet.forEach(({ name, value, options }) =>
+							cookieStore.set(name, value, options),
+						)
+					} catch {
+						// The `setAll` method was called from a Server Component.
+						// This can be ignored if you have middleware refreshing
+						// user sessions.
+					}
+				},
 			},
 		},
-	})
+	)
 }
