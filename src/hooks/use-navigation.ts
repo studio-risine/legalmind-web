2c4ad3cca43279221ebe5c8c3c00bdf900c4b606
Gabriel Melo <gblsmlo@gmail.com>
2025-10-27 19:23:25 -0300

    feat: add useNavigation hook to generate main navigation routes

---
+++ src/hooks/use-navigation.ts
@@ -0,0 +1,78 @@
+import {
+	RiArticleLine,
+	RiChat3Line,
+	RiFolderLine,
+	RiHome9Line,
+	RiPieChart2Line,
+	RiSettings2Line,
+	RiUser6Line,
+	RiWallet2Line,
+} from '@remixicon/react'
+
+function generateMainNavigationRoutes(spaceId: string) {
+	return [
+		{
+			id: 'home',
+			title: 'Home',
+			icon: RiHome9Line,
+			link: `/space/${spaceId}`,
+		},
+		{
+			id: 'processes',
+			title: 'Processos',
+			icon: RiFolderLine,
+			link: `/space/${spaceId}/processos`,
+		},
+		{
+			id: 'publications',
+			title: 'Publicações',
+			icon: RiArticleLine,
+			link: `/space/${spaceId}/publicacoes`,
+		},
+		{
+			id: 'customers-support',
+			title: 'Atendimentos',
+			icon: RiChat3Line,
+			link: `/space/${spaceId}/atendimentos`,
+		},
+		{
+			id: 'clients',
+			title: 'Clientes',
+			icon: RiUser6Line,
+			link: `/space/${spaceId}/clientes`,
+		},
+		{
+			id: 'analytics',
+			title: 'Indicadores',
+			icon: RiPieChart2Line,
+			link: '#',
+		},
+		{
+			id: 'finance',
+			title: 'Finanças',
+			icon: RiWallet2Line,
+			link: `/space/${spaceId}/financas`,
+		},
+		{
+			id: 'settings',
+			title: 'Configurações',
+			icon: RiSettings2Line,
+			link: `/space/${spaceId}/configuracoes`,
+		},
+	]
+}
+
+export function useNavigation() {
+	const mainNavigationRoutes = (spaceId: string) => {
+		if (!spaceId)
+			throw new Error('spaceId is required to generate navigation routes')
+
+		const routes = generateMainNavigationRoutes(spaceId)
+
+		return routes
+	}
+
+	return {
+		mainNavigationRoutes,
+	}
+}
