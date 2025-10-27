3c2fa69188f49dfb04431e7536ed0c6c51888537
Gabriel Melo <gblsmlo@gmail.com>
2025-10-26 21:38:25 -0300

    refactor: update font family to Source Sans 3 in configuration

--- src/config/font-family.ts
+++ src/config/font-family.ts
@@ -1,6 +1,6 @@
-import { Inter } from 'next/font/google'
+import { Source_Sans_3 } from 'next/font/google'
 
-export const fontFamily = Inter({
+export const fontFamily = Source_Sans_3({
 	subsets: ['latin'],
 	display: 'swap',
 })
