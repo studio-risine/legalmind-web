2979c98b41983284419453e6dd63f004ab731d67
Gabriel Melo <gblsmlo@gmail.com>
2025-11-06 20:46:42 -0300

    chore: remove obsolete client dashboard and related components

--- src/app/(private)/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { PageHeaderWithBreadcrumb } from '@modules/dashboard/components/page-header-breadcrumb'
-
-const breadcrumb = [{ label: 'Home', href: '/dashboard' }]
-
-export default function Page() {
-	return <PageHeaderWithBreadcrumb breadcrumb={breadcrumb} />
-}
